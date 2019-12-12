c645062a64b823da60b360a9837a066ba822395b
Fidhi Pradinni <fidhipradinni28@gmail.com>
2019-12-12 15:54:43 -0800

    Bismillah project baru

---
+++ Release/main.d
@@ -0,0 +1,5 @@
+main.d: ../main.c ../myUART/myUART.h ../mySPI/mySPI.h
+
+../myUART/myUART.h:
+
+../mySPI/mySPI.h:
---
+++ Release/mySPI/mySPI.d
@@ -0,0 +1,3 @@
+mySPI/mySPI.d: ../mySPI/mySPI.c ../mySPI/mySPI.h
+
+../mySPI/mySPI.h:
